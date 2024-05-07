3c152fb330632ece405b7647e3414976ec752750
Troidbit <=>
2024-05-06 20:49:54 -0500

    Portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+
+function MyButton() {
+  return (
+      <button>I'm a button</button>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>I'm here to celebrate</h1>
+      <MyButton/>
+   </div>
   );
 }
 
